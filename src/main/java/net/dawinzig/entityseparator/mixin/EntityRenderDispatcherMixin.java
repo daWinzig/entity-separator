8f52cf7a22e1170f90d01dceccfc7bfc2238b6e8
daWinzig <42668927+daWinzig@users.noreply.github.com>
2025-02-02 12:59:01 +0100

    fixed typo

--- src/main/java/net/dawinzig/entityseparator/mixin/EntityRenderDispatcherMixin.java
+++ src/main/java/net/dawinzig/entityseparator/mixin/EntityRenderDispatcherMixin.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Mixin(EntityRenderDispatcher.class)
-public class EntityRendererDispatcherMixin {
+public class EntityRenderDispatcherMixin {
     @Unique
     Entity current = null;
 
